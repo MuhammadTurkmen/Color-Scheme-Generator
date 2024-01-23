593ae22600d6645df74eac8db13cf32c89f1bc24
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-01-23 11:11:47 +0430

    adding .slice

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const colorBtn = document.getElementById('color-btn');
 
 colorBtn.addEventListener('click', function()   {
     const head = document.getElementById("head").value.slice(1)
+    const selectColor = document.getElementById("select-color").value
 
 
 })

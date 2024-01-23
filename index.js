e2a317e5058750aa510540b7b4ddb9ef7fdc68e7
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-01-23 11:12:35 +0430

    adding fetch

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ colorBtn.addEventListener('click', function()   {
     const head = document.getElementById("head").value.slice(1)
     const selectColor = document.getElementById("select-color").value
 
+    fetch(`https://www.thecolorapi.com/scheme?hex=${head}&mode=${selectColor.toLowerCase()}&count=5`)
+
 
 })
